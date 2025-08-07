9792e31a18d491b3cf4bfd17d948e20a9aedb1c3
MarioRodaNL <147842740+MarioRodaNL@users.noreply.github.com>
2025-08-07 20:36:06 -0300

    Create FlipFlop.py

---
+++ FlipFlop.py
@@ -0,0 +1,10 @@
+for i in range(101):
+  if i % 3 == 0 and i % 5 == 0 and i !=0
+    print("FlipFlop")
+  elif i % 3 == 0 and i != 0:
+    print("Flip")
+  elif i % 5 == 0 and i != 0:
+    print("Flop")
+  else:
+    print(i)
+    
